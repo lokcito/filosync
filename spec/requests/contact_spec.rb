2c891aa96b25c33e9183dd4eef2c7b1185648a87
Ray Rojas <enciso.leo@gmail.com>
2021-10-17 12:09:29 +0000

    deploy

--- spec/requests/contact_spec.rb
+++ spec/requests/contact_spec.rb
@@ -2,6 +2,14 @@ require 'rails_helper'
 
 RSpec.describe "Contacts", type: :request do
   describe "GET /index" do
+    before(:each) do
+			@user = User.create(
+				email: 'test@example.com', 
+				password: 'password123',
+				password_confirmation: 'password123'
+			)
+      sign_in @user
+    end
     it "returns http success" do
       get "/contact"
       expect(response).to have_http_status(:success)
